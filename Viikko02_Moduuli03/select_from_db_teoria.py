c706e5571f6624bb3f80121cd4c465dcbdddbbb8
Tontsa <tontsa@nic.fi>
2024-09-13 13:49:41 +0300

    Updated

--- Viikko02_Moduuli03/select_from_db_teoria.py
+++ Viikko02_Moduuli03/select_from_db_teoria.py
@@ -1,4 +1,4 @@
-# Select from db - Ohjelmisto 1 Tietokannat - Teoria vko 2
+# Select from db - Ohjelmisto 1 Tietokannat - Teoria vko 2 moduuli 3
 import mysql.connector
 
 def tee_kysely(sql_lauseke):
